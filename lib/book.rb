83da2b36310727634feb939c7d80928a0381c2e3
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-20 19:16:26 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,38 @@
+class Book
+    def initialize(title)
+        @title = title
+    end
 
+    # Properties
+    def title=(value)
+        @title = value
+    end
+    def title
+        @title
+    end
 
+    def author=(value)
+        @author = value
+    end
+    def author
+        @author
+    end
+
+    def page_count=(value)
+        @page_count = value
+    end
+    def page_count
+        @page_count
+    end
+    def genre=(value)
+        @genre = value
+    end
+    def genre
+        @genre
+    end
+
+    # Method
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,53 @@
 # Make your shoe class here!
+class Shoe
+    # Initialize Brand
+    def initialize(brand)
+        @brand = brand
+    end
+
+    # Properties
+    def brand=(value)
+        @brand= value
+    end
+
+    def brand
+        @brand
+    end
+
+    def color=(value)
+        @color= value
+    end
+    def color
+        @color
+    end
+
+    def size=(value)
+        @size= value
+    end
+    def size 
+        @size
+    end
+
+    def material=(value)
+        @material= value
+    end
+    def material
+        @material
+    end
+
+    def condition=(value)
+        @condition = value
+    end
+    def condition
+        @condition
+    end
+
+    # Method 
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = ("new")
+    end
+end
+
+shoe = Shoe.new('Nike')
+shoe.condition
